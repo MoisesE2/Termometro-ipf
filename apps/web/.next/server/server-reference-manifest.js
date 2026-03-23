2331ddeb89855330638082f9cf83b33e3c1193bd
Moisés Eduardo <118224290+MoisesE2@users.noreply.github.com>
2026-03-23 15:52:54 -0300

    autalizando

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BLQWT7TQNicCECHpAfzXENO75ZBEWxK9bsZXYXlkdmI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
